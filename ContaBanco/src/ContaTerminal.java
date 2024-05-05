4797f22d33cda3a0e9a9d6113d5202202b711418
Lara Scremin <lscreminmendes@gmail.com>
2024-05-05 16:30:25 -0300

    desafio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o numero da sua conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite sua agencia: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o seu saldo: ");
+        double saldo = scanner.nextDouble();
+
+        // imprimindo os dados obtidos pelo usuario
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque");
 
     }
 
