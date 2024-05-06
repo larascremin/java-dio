6014066248ca582e954e2ad98d4002e170faab09
Lara Scremin <lscreminmendes@gmail.com>
2024-05-06 14:53:16 -0300

    aula de condicionais

---
+++ aulas/src/condicional/SwitchCase.java
@@ -0,0 +1,25 @@
+package condicional;
+
+public class SwitchCase {
+
+    public static void main(String[] args) {
+
+        int numero = 10;
+
+        switch (numero) {
+            case 2: {
+                System.out.println(numero);
+                break;
+            }
+            case 6: {
+                System.out.println(numero);
+                break;
+            }
+            default: {
+                System.out.println("indefinido");
+            }
+        }
+
+    }
+
+}
---
+++ aulas/src/condicional/Ternario.java
@@ -0,0 +1,14 @@
+package condicional;
+
+public class Ternario {
+
+    public static void main(String[] args) {
+
+        int nota = 7;
+
+        String resultado = nota >= 7 ? "aprovado" : "reprovado";
+
+        System.out.println(resultado);
+    }
+
+}
